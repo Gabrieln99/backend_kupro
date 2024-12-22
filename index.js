c9aa94e0052abc89de9aedfd390da65dd7e653c7
Gabrieln999 <155887339+Gabrieln99@users.noreply.github.com>
2024-12-22 16:27:34 +0100

    uspjesno configurirana baza-MongoAtlas_db

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import { connectToDatabase } from "./db.js";
+const app = express();
+let db = await connectToDatabase();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("KuPro");
+});
+
+app.get('/kate', async (req, res) => {
+    let kategorije_collection = db.collection('kategorije'); 
+    let allKategorije = await kategorije_collection.find().toArray(); 
+    res.status(200).json(allKategorije);
+    });
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log("Greška prilikom pokretanja servera", error);
+  }
+  console.log(` poslužitelj dela na http://localhost:${PORT}`);
+});
