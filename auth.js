ff831218edd9ee1ea53548d44781deb05831453e
Udolino04 <mudovici1@unipu.hr>
2024-12-23 12:35:21 +0100

    /login /singup rijenei treba provjerit dal delaju kako rabe

--- auth.js
+++ auth.js
@@ -3,12 +3,17 @@
 
   const router = express.Router();
 
+// nebi skodilo napravit folder ruter di cemo odvajat APIJe(/) na temelju cemu nan treba tipo sad za log in sing up da je u posebm ruteru da se ne mjesa jer cemo imat puno /
+// provjeri smao 'korisnici' dal san dobro napisa ili kako vec si ti napisa ne moren provjerit dal dela jer ne moren otprit tvoj mongodb 
+// sad neman bas vremena proucit kako da se spojin na taj mongodb pa samo ti kad rivas provjeri i tako u komentar mi stavi dal dela ili me zovi 
+// testirat u postamnu obavezno ili tunder clientu 
+
   router.post('/signup', async (req, res) => {
       const { ime, prezime, email, lozinka } = req.body;
       const db = await connectToDatabase();
     
       try {
-          const existingUser = await db.collection('users').findOne({ email });
+          const existingUser = await db.collection('korisnici').findOne({ email });
         
           if (existingUser) {
               return res.status(400).json({ message: 'Korisnik vec postoji' });
